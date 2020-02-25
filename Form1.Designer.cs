60cc747dd0c2227ed1f83136637b321c5454c607
Oleg <legich007@mail.ru>
2020-02-25 21:14:43 +0300

    add files

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace TestADO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.geoSystemDBDataSet1 = new TestADO.GeoSystemDBDataSet();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.geoSystemDBDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 90);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(500, 400);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(690, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(58, 511);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(393, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(649, 90);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(500, 400);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(864, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 50);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Show";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(157, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 50);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Select";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // geoSystemDBDataSet1
+            // 
+            this.geoSystemDBDataSet1.DataSetName = "GeoSystemDBDataSet";
+            this.geoSystemDBDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(690, 511);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(445, 24);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1161, 558);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GeoData";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.geoSystemDBDataSet1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private GeoSystemDBDataSet geoSystemDBDataSet1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TestADO
+{
+    public partial class Form1 : Form
+    {
+        static SqlConnection GeoConnection = new SqlConnection(@"Data Source=HOME-PC;Initial Catalog=GeoSystemDB;Integrated Security=True");
+        static string query = "SELECT * FROM [Geodesist_geosystem_schema].[Geodesy]";
+        static SqlDataAdapter SqlDataAdapter1 = new SqlDataAdapter(query, GeoConnection);
+        DataSet GeoDataset = new DataSet("Geodesy");
+        SqlCommandBuilder commands = new SqlCommandBuilder(SqlDataAdapter1);
+   
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            GetTableNames();
+            comboBox1.Text = comboBox1.Items[0].ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter1.Fill(GeoDataset, "[Geodesist_geosystem_schema].[Geodesy]");
+            dataGridView1.DataSource = GeoDataset.Tables["[Geodesist_geosystem_schema].[Geodesy]"];
+            
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string str = GetCurrentTable(comboBox1.Text);
+                string query = "SELECT * FROM " + str;
+                SqlDataAdapter SqlDataAdapter1 = new SqlDataAdapter(query, GeoConnection);
+                SqlCommandBuilder commands = new SqlCommandBuilder(SqlDataAdapter1);
+                commands.DataAdapter.Update(GeoDataset.Tables[str]);
+            }
+            catch (Exception a)
+            {
+
+                MessageBox.Show(a.ToString());
+            }
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                string str = GetCurrentTable(comboBox1.Text);
+                string query = "SELECT * FROM " + str;
+                SqlDataAdapter SqlDataAdapter1 = new SqlDataAdapter(query, GeoConnection);
+                SqlCommandBuilder commands = new SqlCommandBuilder(SqlDataAdapter1);
+                GeoDataset.EndInit();
+                var index = dataGridView1.CurrentRow.Index;
+                GeoDataset.Tables[str].Rows[index].Delete();
+                SqlDataAdapter1.Update(GeoDataset.Tables[str]);
+            }
+            catch(Exception a)
+            {
+                MessageBox.Show(a.ToString());
+            }
+                
+        }
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            string str = (string)dataGridView1.Columns[dataGridView1.CurrentCell.ColumnIndex].HeaderText;
+            
+            DataRelationCollection dr = geoSystemDBDataSet1.Relations;
+
+            foreach(DataRelation temp in dr)
+            {
+                
+                if(temp.ChildTable.TableName == comboBox1.Text)
+                {
+                    if(temp.ChildColumns[0].ColumnName == str)
+                    {
+                        comboBox2.Items.Add(temp.ParentTable.TableName);
+                        /*string tableStr = GetCurrentTable(temp.ParentTable.TableName);
+                        string query = "SELECT * FROM " + tableStr + " WHERE " + temp.ParentColumns[0].ColumnName + " = " + dataGridView1.CurrentCell.Value;
+                        SqlDataAdapter SqlDataAdapter1 = new SqlDataAdapter(query, GeoConnection);
+                        SqlCommandBuilder commands = new SqlCommandBuilder(SqlDataAdapter1);
+                        GeoDataset = new DataSet("Geo");
+                        commands.DataAdapter.Fill(GeoDataset, tableStr);
+                        dataGridView2.DataSource = GeoDataset.Tables[tableStr];*/
+                        break;
+                    }
+                }
+                else if (temp.ParentTable.TableName == comboBox1.Text && !comboBox2.Items.Contains(temp.ChildTable.TableName))
+                {
+                    comboBox2.Items.Add(temp.ChildTable.TableName);
+                    /*string tableStr = GetCurrentTable(temp.ChildTable.TableName);
+                    string query = "SELECT * FROM " + tableStr + " WHERE " + temp.ChildColumns[0].ColumnName + " = " + dataGridView1.CurrentCell.Value;
+                    SqlDataAdapter SqlDataAdapter1 = new SqlDataAdapter(query, GeoConnection);
+                    SqlCommandBuilder commands = new SqlCommandBuilder(SqlDataAdapter1);
+                    GeoDataset = new DataSet("Geodesy");
+                    commands.DataAdapter.Fill(GeoDataset, tableStr);
+                    dataGridView2.DataSource = GeoDataset.Tables[tableStr];*/
+                  
+
+                }
+                
+            }
+
+            comboBox2.Text = comboBox2.Items[0].ToString();
+        }
+
+        public string GetCurrentColumnName()
+        {
+            DataRelationCollection dr = geoSystemDBDataSet1.Relations;
+            foreach (DataRelation temp in dr)
+            {
+                if (temp.ParentTable.TableName == comboBox1.Text && temp.ChildTable.TableName == comboBox2.Text)
+                {
+
+
+                    return temp.ChildColumns[0].ColumnName;
+
+
+
+                }
+                else if (temp.ChildTable.TableName == comboBox1.Text && temp.ParentTable.TableName == comboBox2.Text)
+                    return temp.ParentColumns[0].ColumnName;
+
+
+            }
+            return "";
+
+        }
+        public void GetTableNames()
+        {
+            comboBox1.Items.Clear();
+            GeoConnection.Open();
+            DataTable dt = GeoConnection.GetSchema("Tables");
+            GeoConnection.Close();
+            foreach(DataRow dr in dt.Rows)
+            {
+                if(dr.ItemArray[3].ToString()=="BASE TABLE"&&dr.ItemArray[2].ToString()!="sysdiagrams")
+                {
+                    comboBox1.Items.Add(dr["TABLE_NAME"]);
+                }
+               
+                
+            }
+            Console.WriteLine();
+        }
+
+        public string GetCurrentTable(string str)
+        {
+            GeoConnection.Open();
+            DataTable dt = GeoConnection.GetSchema("Tables");
+            GeoConnection.Close();
+            foreach (DataRow dr in dt.Rows)
+            {
+                if (dr.ItemArray[2].ToString().Equals(str))
+                {
+                    string temp = "[" + dr.ItemArray[1].ToString() + "]." + "[" + dr.ItemArray[2].ToString() + "]";
+                    return temp;
+                }
+                
+
+            }
+            return "";
+        }
+
+        public void Clear(DataGridView dataGridView)
+        {
+            
+            while (dataGridView.Rows.Count > 1)
+                for (int i = 0; i < dataGridView.Rows.Count - 1; i++)
+                    dataGridView.Rows.Remove(dataGridView.Rows[i]);
+        }
+        
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clear(dataGridView1);
+                string str = GetCurrentTable(comboBox1.Text);
+                string query = "SELECT * FROM " + str;
+                SqlDataAdapter SqlDataAdapter1 = new SqlDataAdapter(query, GeoConnection);
+                SqlCommandBuilder commands = new SqlCommandBuilder(SqlDataAdapter1);
+                GeoDataset = new DataSet("Geodesy");
+                commands.DataAdapter.Fill(GeoDataset, str);
+                dataGridView1.DataSource = GeoDataset.Tables[str];
+            }
+            catch (Exception a)
+            {
+
+                MessageBox.Show(a.ToString());
+            }
+        }
+
+       
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Clear(dataGridView2);
+                string str = GetCurrentTable(comboBox2.Text);
+                string query = "SELECT * FROM " + str + " WHERE " + GetCurrentColumnName() + " = " + dataGridView1.CurrentCell.Value;
+                SqlDataAdapter SqlDataAdapter1 = new SqlDataAdapter(query, GeoConnection);
+                SqlCommandBuilder commands = new SqlCommandBuilder(SqlDataAdapter1);
+                GeoDataset = new DataSet("G");
+                commands.DataAdapter.Fill(GeoDataset, str);
+                dataGridView2.DataSource = GeoDataset.Tables[str];
+
+
+            }
+            catch (Exception a)
+            {
+
+                MessageBox.Show(a.ToString());
+            }
+        }
+    }
+}
